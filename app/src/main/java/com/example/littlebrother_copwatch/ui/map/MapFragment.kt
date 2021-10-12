e55beeb5502303db4d769033c533170e29f7c8c2
bpp-ra <91578353+bpp-ra@users.noreply.github.com>
2021-10-12 04:37:50 -0400

    MapFragment.kt: Lines 53-103 | Added coords as well as markers to locations of Atlanta Police Stations in Zones 1-6. Precincts include, Zone 1, Zone 2, Zone 3 (Mini Grant Park Precinct), Zone 4 (Cascade Cir. Precinct), Zone 5, Zone 6 (Main Precinct and Mini Precinct). Next commit will add custom marker icons to precinct locations to make them more distinguishable.
    Lines 105-114 | Added a block of sample code to test the plausibility of adding polygons to map surrounding Atlanta and/or Atlanta's Police Zones

--- app/src/main/java/com/example/littlebrother_copwatch/ui/map/MapFragment.kt
+++ app/src/main/java/com/example/littlebrother_copwatch/ui/map/MapFragment.kt
@@ -39,6 +39,7 @@ class MapFragment : Fragment() , OnMapReadyCallback {
         map?.let{
             googleMap = it
         }
+
         val atlanta = LatLng(33.778, -84.388)
         map?.addMarker(
             MarkerOptions()
@@ -49,6 +50,68 @@ class MapFragment : Fragment() , OnMapReadyCallback {
         )
         map?.moveCamera(CameraUpdateFactory.newLatLngZoom(atlanta, 10F))
 
+        //INFO TODO: The following sets coordinates and marker positions for Atlanta Police related facilities
+        val zoneOne = LatLng(33.777, -84.460)
+        map?.addMarker(
+            MarkerOptions()
+                .position(zoneOne)
+                .title("Zone 1 Atlanta Police Department")
+        )
+
+        val zoneTwo = LatLng(33.840, -84.372)
+        map?.addMarker(
+            MarkerOptions()
+                .position(zoneTwo)
+                .title("Zone 2 Atlanta Police Department")
+        )
+
+        val zoneThree = LatLng(33.730, -84.373)
+        map?.addMarker(
+            MarkerOptions()
+                .position(zoneThree)
+                .title("Zone 3 Atlanta Police Department")
+        )
+
+        val zoneFour = LatLng(33.725, -84.450)
+        map?.addMarker(
+            MarkerOptions()
+                .position(zoneFour)
+                .title("Zone 4 Atlanta Police Department")
+        )
+
+        val zoneFive = LatLng(33.760, -84.389)
+        map?.addMarker(
+            MarkerOptions()
+                .position(zoneFive)
+                .title("Zone 5 Atlanta Police Department")
+        )
+
+        val zoneSix = LatLng(33.751, -84.321)
+        map?.addMarker(
+            MarkerOptions()
+                .position(zoneSix)
+                .title("Zone 6 Atlanta Police Department")
+        )
+
+        val zoneSixMini = LatLng(33.765, -84.349)
+        map?.addMarker(
+            MarkerOptions()
+                .position(zoneSixMini)
+                .title("Mini Zone 6 Atlanta Police Department")
+        )
+
+        //INFO TODO: END
+
+        //TODO: Add polygons to visualize boundaries for City of Atlanta and/or Atlanta Police Zones
+        /*
+        val zone_one = map?.addPolygon(
+            PolygonOptions()
+                .clickable(true)
+                .add(
+                    LatLng(),
+                )
+        )
+        */
     }
 
 }
