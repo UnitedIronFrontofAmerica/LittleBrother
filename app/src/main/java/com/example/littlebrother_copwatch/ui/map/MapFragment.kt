01eaaefa1a8cf5716e0240a6c4dc6f522cbac320
bpp-ra <91578353+bpp-ra@users.noreply.github.com>
2021-10-14 11:00:42 -0400

    MapFragment.kt: Added custom icons to Atlanta Police Facility locations. police_facility_marker.png: Changed the size of the icon to 30px

--- app/src/main/java/com/example/littlebrother_copwatch/ui/map/MapFragment.kt
+++ app/src/main/java/com/example/littlebrother_copwatch/ui/map/MapFragment.kt
@@ -50,12 +50,14 @@ class MapFragment : Fragment() , OnMapReadyCallback {
         )
         map?.moveCamera(CameraUpdateFactory.newLatLngZoom(atlanta, 10F))
 
-        //INFO TODO: The following sets coordinates and marker positions for Atlanta Police related facilities
+        //INFO: The following sets coordinates and marker positions for Atlanta Police related facilities
+        //TODO: More facilities needed
         val zoneOne = LatLng(33.777, -84.460)
         map?.addMarker(
             MarkerOptions()
                 .position(zoneOne)
                 .title("Zone 1 Atlanta Police Department")
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.police_facility_marker))
         )
 
         val zoneTwo = LatLng(33.840, -84.372)
@@ -63,6 +65,7 @@ class MapFragment : Fragment() , OnMapReadyCallback {
             MarkerOptions()
                 .position(zoneTwo)
                 .title("Zone 2 Atlanta Police Department")
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.police_facility_marker))
         )
 
         val zoneThree = LatLng(33.730, -84.373)
@@ -70,6 +73,7 @@ class MapFragment : Fragment() , OnMapReadyCallback {
             MarkerOptions()
                 .position(zoneThree)
                 .title("Zone 3 Atlanta Police Department")
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.police_facility_marker))
         )
 
         val zoneFour = LatLng(33.725, -84.450)
@@ -77,6 +81,7 @@ class MapFragment : Fragment() , OnMapReadyCallback {
             MarkerOptions()
                 .position(zoneFour)
                 .title("Zone 4 Atlanta Police Department")
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.police_facility_marker))
         )
 
         val zoneFive = LatLng(33.760, -84.389)
@@ -84,6 +89,7 @@ class MapFragment : Fragment() , OnMapReadyCallback {
             MarkerOptions()
                 .position(zoneFive)
                 .title("Zone 5 Atlanta Police Department")
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.police_facility_marker))
         )
 
         val zoneSix = LatLng(33.751, -84.321)
@@ -91,6 +97,7 @@ class MapFragment : Fragment() , OnMapReadyCallback {
             MarkerOptions()
                 .position(zoneSix)
                 .title("Zone 6 Atlanta Police Department")
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.police_facility_marker))
         )
 
         val zoneSixMini = LatLng(33.765, -84.349)
@@ -98,6 +105,7 @@ class MapFragment : Fragment() , OnMapReadyCallback {
             MarkerOptions()
                 .position(zoneSixMini)
                 .title("Mini Zone 6 Atlanta Police Department")
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.police_facility_marker))
         )
 
         //INFO TODO: END
@@ -112,6 +120,7 @@ class MapFragment : Fragment() , OnMapReadyCallback {
                 )
         )
         */
+        //TODO: END
     }
 
 }
